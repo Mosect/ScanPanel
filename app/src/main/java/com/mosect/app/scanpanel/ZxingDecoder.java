b6a64d9442c581fb827939366ef5f33b522d13c2
mosect <mosect@outlook.com>
2023-01-27 20:47:30 +0800

    V1.0.2

--- app/src/main/java/com/mosect/app/scanpanel/ZxingDecoder.java
+++ app/src/main/java/com/mosect/app/scanpanel/ZxingDecoder.java
@@ -1,9 +1,6 @@
 package com.mosect.app.scanpanel;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
-import android.graphics.YuvImage;
 
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.MultiFormatReader;
@@ -11,8 +8,6 @@ import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
 import com.mosect.lib.scanpanel.coder.FrameDecoder;
-
-import java.io.ByteArrayOutputStream;
 
 public class ZxingDecoder implements FrameDecoder {
 
@@ -24,12 +19,6 @@ public class ZxingDecoder implements FrameDecoder {
 
     @Override
     public String decodeFrame(int format, byte[] data, int width, int height, Rect clip) throws Exception {
-//        YuvImage image = new YuvImage(data, format, width, height, new int[]{width, width});
-//        ByteArrayOutputStream temp = new ByteArrayOutputStream(512);
-//        image.compressToJpeg(clip, 100, temp);
-//        byte[] jpeg = temp.toByteArray();
-//        Bitmap bm = BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length);
-//        bm.recycle();
         int cl = 0, ct = 0, cw = width, ch = height;
         if (null != clip) {
             cl = clip.left;
